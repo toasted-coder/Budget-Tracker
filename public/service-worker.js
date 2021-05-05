050cdbda2c5a603b7675e8f06dccc62372e190df
toasted-coder <christmanholmes21@gmail.com>
2021-05-05 10:00:27 -0700

    error when deploying, added some syntax to cache provided images. hopefully fixes it

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,6 +7,8 @@ const FILES_TO_CACHE = [
   "/styles.css",
   "/index.js",
   "/db.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
 
